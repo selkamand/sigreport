3106fd18db3affb307a3857ecf87960ab5b269ae
Sam El-Kamand <sam.elkamand@gmail.com>
2024-10-12 08:55:57 +1100

    fix: sigstory placeholder function added

--- R/create_report.R
+++ R/create_report.R
@@ -1,7 +1,10 @@
-sig_story <- function(observed, model, aetiology, similar_samples){
-
-}
-
-model_diagrams <- function(){
-
+#' Sigstory
+#'
+#' @param sigstory a 'signature_analysis' s3 class object containing
+#' summarised results from a mutational signature analysis.
+#' @return Run for its side effects. Invisibly returns NULL.
+#' @export
+#'
+sig_story <- function(sigstory, outdir){
+  invisible(NULL)
 }
