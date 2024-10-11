671303ad8ef7e0dafe80cd33b1d8625d76ac1db5
Sam El-Kamand <sam.elkamand@gmail.com>
2024-10-12 08:57:41 +1100

    fix: tests now use correct package name
    also added roxygen comment for outdir param

--- R/create_report.R
+++ R/create_report.R
@@ -2,6 +2,7 @@
 #'
 #' @param sigstory a 'signature_analysis' s3 class object containing
 #' summarised results from a mutational signature analysis.
+#' @param outdir output directory where html reports should be saved
 #' @return Run for its side effects. Invisibly returns NULL.
 #' @export
 #'
--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/articles/special-files.html
 
 library(testthat)
-library(sigreport)
+library(sigstory)
 
-test_check("sigreport")
+test_check("sigstory")
